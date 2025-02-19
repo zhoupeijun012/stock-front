d0798721618ff59c24b1bb792ce1928361e2cf7a
zhoupeijun012 <zhoupeijun012@163.com>
2025-02-19 16:16:23 +0800

    优化

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,7 @@ import httpRequest from '@/utils/http';
 export const getStockList = (params)=>{
   return httpRequest.post('/getStockList',params)
 }
+
+export const getEtfList = (params)=>{
+  return httpRequest.post('/getEtfList',params)
+}
