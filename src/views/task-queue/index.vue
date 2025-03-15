63aa4034171626ab1dab344cf7deac0fbf9363f2
zhoupeijun012 <zhoupeijun012@163.com>
2025-03-15 21:20:50 +0800

    优化

--- src/views/task-queue/index.vue
+++ src/views/task-queue/index.vue
@@ -21,7 +21,7 @@ export default {
       loading: false,
       options: {
         columns: [
-                    {
+          {
             prop: "taskLevel",
             label: "优先级",
             width: "90px",
@@ -31,48 +31,40 @@ export default {
           {
             prop: "taskName",
             label: "任务名称",
-            width: "150px",
             showOverflowTooltip: true,
           },
 
           {
             prop: "modelName",
             label: "模型名称",
-            width: "100px",
             showOverflowTooltip: true,
           },
           {
             prop: "modelFunc",
             label: "调用方法",
-            width: "100px",
             showOverflowTooltip: true,
           },
           {
             prop: "taskParams",
             label: "使用参数",
-            minWidth: "100px",
             showOverflowTooltip: true,
           },
-
 
           {
             prop: "message",
             label: "错误原因",
-            minWidth: "150px",
             showOverflowTooltip: true,
           },
-                    {
+          {
             prop: "retryCount",
             label: "重试次数",
-            width: "90px",
-            align: 'center',
+            align: "center",
             showOverflowTooltip: true,
           },
-                    {
+          {
             prop: "needRetry",
             label: "是否重试",
-            width: "90px",
-            align: 'center',
+            align: "center",
             formatter: (row) => {
               return row.needRetry == "1" ? "是" : "否";
             },
