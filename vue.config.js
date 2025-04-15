15a172ddc73b661a99b6ab2dd1cc304d789e5aa1
zhoupeijun012 <zhoupeijun012@163.com>
2025-04-16 01:03:10 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   lintOnSave: false,
   devServer: {
     // proxy: 'http://localhost:12345',
-    proxy: "http://www.amdyes.asia",
+    proxy: "https://www.amdyes.asia",
     client: {
       overlay: false,
     },
