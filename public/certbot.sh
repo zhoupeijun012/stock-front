1525bdbda47c824c38c07c14644c5281a935f0f4
zhoupeijun012 <zhoupeijun012@163.com>
2025-04-15 18:00:47 +0800

    2323

---
+++ public/certbot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 确保 Nginx 服务正在运行
+nginx -t && nginx
+
+# 获取或更新证书
+certbot --nginx -d www.chives.asia --non-interactive --agree-tos --email zhoupeijun012@163.com
+
+# 启动一个定时任务来定期更新证书
+while true; do
+    sleep 12h
+    certbot renew --nginx --non-interactive
+done
